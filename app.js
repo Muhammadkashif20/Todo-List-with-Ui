019b6cd7620151afc0f023ee2280f310a4a3b71e
muhammadkashif20 <kashif28@gmail.com>
2024-06-26 20:26:24 +0500

    changing and ubdating

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function addBtn() {
     let show=document.getElementById('show')
     show.innerHTML+=`<ul>
   <li id="items" class="outline-2 border-2 outline-gray-200 border-gray-200 w-[25rem] h-[7vh] px-2 my-6 rounded-md flex justify-between items-center" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
-    <span class="flex-grow">${inp}</span>
+    <span class="flex-grow ">${inp}</span>
     <button id="delbtn" onclick="del()" class="bg-gray-200 w-[5rem] h-[6.8vh] rounded-r-md font-bold mr-[-.5rem] flex-shrink-0">Delete</button>
   </li>
 </ul>`
